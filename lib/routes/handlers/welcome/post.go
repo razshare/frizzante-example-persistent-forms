618825c395283a9156a3f84d646cfc50a4592e57
Razvan <razvan@razshare.dev>
2025-09-26 13:08:49 +0200

    chore(app): resetting input if it's valid.

--- lib/routes/handlers/welcome/post.go
+++ lib/routes/handlers/welcome/post.go
@@ -9,11 +9,16 @@ import (
 
 func Post(client *client.Client) {
 	var errorMessage string
+
 	state := session.Start(receive.SessionId(client))
 	username := receive.FormValue(client, "username")
 
 	if len(username) < 3 {
+		// Explain why the input is invalid.
 		errorMessage = "username is too short"
+	} else {
+		// If the input is valid, reset it.
+		username = ""
 	}
 
 	state.Form = session.Form{
